f0fd80ccfc3cbd77c3ea36f46ba0c76a6e8d835b
Hyunjoon Kim <joonkim0625@gmail.com>
2019-01-27 15:23:35 +0900

    initial

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import "./About.scss";
 
 export default class About extends Component {
   render() {
-    return <div className="about">저는 누구입니다!</div>;
+    return (
+      <div className="about">
+        <p>
+          이제는 일상에서 빼놓을 수 없는, 무엇인가에 대한 정보를 가장 먼저
+          접하게 되는 웹사이트를 만든다는 것에 매력을 느껴 개발을 시작하게 된
+          김현준이라고 합니다.
+          <br /> 매일 성장하고 현실에 안주하지 않는 개발자가 되려고 노력하고
+          있습니다.
+        </p>
+      </div>
+    );
   }
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,99 +3,126 @@ import "./Portfolio.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
+
+import {
+  Scroll,
+  DirectLink,
+  Link,
+  Events,
+  Element,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller
+} from "react-scroll";
 
 export default class Portfolio extends Component {
   render() {
     return (
       <div className="portfolios">
-        <div className="item1">
-          <div className="title">
-            <strong>조기요 V2</strong>
-          </div>
-          <p>
-            패스트캠퍼스 프론트엔드 개발 스쿨 수료 프로젝트 후, 제가 혼자 몇
-            가지 기능을 추가했습니다.
-          </p>
-          <iframe
-            title="jogiyo-v2"
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/AELNQ2UShRk"
-            frameBorder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
-          <div className="description">
+        {/* first item */}
+        <Element name="item1">
+          <div className="item1">
+            <div className="title">
+              <strong>조기요 V2</strong>
+            </div>
             <p>
-              반응형 레이아웃, IE11 지원을 위한 폴리필 적용, 로그인(hello /
-              1234) 후 리뷰 작성 및 삭제 가능
+              패스트캠퍼스 프론트엔드 스쿨 협업 프로젝트 후, 제가 혼자 몇 가지
+              기능을 추가했습니다.
             </p>
-            <div className="projectWrap scale">
+            <iframe
+              title="jogiyo-v2"
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/AELNQ2UShRk"
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+            <div className="description">
+              <p>
+                반응형 레이아웃, IE11 지원을 위한 폴리필 적용, 로그인(hello /
+                1234) 후 리뷰 작성 및 삭제 가능
+              </p>
+              <div className="projectWrap scale">
+                <a
+                  className="projectIssues"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/joonkim0625/jogiyo-v2/projects/1"
+                >
+                  프로젝트 관련 이슈는 이곳에 정리했습니다
+                </a>
+              </div>
+              <p>React, React-router, axios, Scss, HTML, CSS, netlify</p>
+            </div>
+            <div className="icons1">
               <a
-                className="projectIssues"
                 target="_blank"
                 rel="noopener noreferrer"
-                href="https://github.com/joonkim0625/jogiyo-v2/projects/1"
+                href="https://github.com/joonkim0625/jogiyo-v2"
               >
-                프로젝트 관련 이슈는 이곳에 정리했습니다
+                <FontAwesomeIcon icon={faGithub} color={"black"} />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://jogiyo2.netlify.com/"
+              >
+                <FontAwesomeIcon icon={faHome} color={"black"} />
               </a>
             </div>
-            <p>React, React-router, axios, Scss, HTML, CSS, netlify</p>
+            <Link to="item2" spy={true} smooth={true} duration={500}>
+              <p className="toProject">다음 프로젝트</p>
+            </Link>
           </div>
-          <div className="icons1">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/joonkim0625"
-            >
-              <FontAwesomeIcon icon={faGithub} color={"black"} />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://jogiyo2.netlify.com/"
-            >
-              <FontAwesomeIcon icon={faHome} color={"black"} />
-            </a>
-          </div>
-        </div>
+        </Element>
 
         {/* second item */}
-        <div className="item2">
-          <div className="title">
-            <strong>오목 게임 만들기 프로젝트</strong>
-          </div>
-          <p>학원 내 프로젝트 종료 후, 제가 기능과 디자인을 추가했습니다.</p>
+        <Element name="item2">
+          <div className="item2">
+            <div className="title">
+              <strong>오목 게임 만들기 프로젝트</strong>
+            </div>
+            <p>
+              패스트캠퍼스 협업 프로젝트 종료 후, 제가 기능과 약간의 디자인을
+              추가했습니다.
+            </p>
 
-          <img
-            className="omokImg"
-            src="https://i.postimg.cc/jdcYXcGZ/2019-01-09-11-29-28.png"
-            alt="오목 게임 이미지"
-          />
-          <p>
-            턴에 따라 바둑판 위의 커서 색이 플레이어의 돌의 색으로 바뀜, 게임
-            종료 후, 'restart'를 위해 엔터키 사용 가능, 무르기 기능의 추가.
-          </p>
-          <p>HTML, CSS, JavaScript</p>
+            <img
+              className="omokImg"
+              src="https://i.postimg.cc/jdcYXcGZ/2019-01-09-11-29-28.png"
+              alt="오목 게임 이미지"
+            />
+            <p>턴에 따라 바둑판 위의 커서 색이 플레이어의 돌의 색으로 바뀜.</p>
+            <p>
+              {" "}
+              게임 종료 후, 'restart'를 위해 엔터키 사용 가능, 무르기 기능의
+              추가.
+            </p>
 
-          <div className="icons1">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/joonkim0625"
-            >
-              <FontAwesomeIcon icon={faGithub} color={"black"} />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://jogiyo2.netlify.com/"
-            >
-              <FontAwesomeIcon icon={faHome} color={"black"} />
-            </a>
+            <p>HTML, CSS, JavaScript</p>
+
+            <div className="icons1">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/joonkim0625/team-guobaorou-omok"
+              >
+                <FontAwesomeIcon icon={faGithub} color={"black"} />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://joons-omok.netlify.com"
+              >
+                <FontAwesomeIcon icon={faHome} color={"black"} />
+              </a>
+            </div>
+            <Link to="item1" spy={true} smooth={true} duration={500}>
+              <p className="toProject">이전 프로젝트</p>
+            </Link>
           </div>
-        </div>
+        </Element>
       </div>
     );
   }
